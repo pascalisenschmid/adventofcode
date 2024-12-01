d61317ab48541781ec907aa79f5c4f752d0d8264
Pascal Isenschmid <pascal@isenschmid.com>
2024-12-01 13:08:43 +0100

    boostrap 2024

---
+++ 2024/flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    ocaml-overlay = {
+      url = "github:nix-ocaml/nix-overlays";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ocaml-overlay, ... }:
+        let
+          inherit (pkgs) ocamlPackages mkShell;
+          inherit (ocamlPackages) buildDunePackage;
+          name = "aoc_2024";
+          version = "0.0.1";
+        in {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ inputs.ocaml-overlay.overlays.default ];
+          };
+          devShells = {
+            default = mkShell {
+              buildInputs = [
+                pkgs.just
+                ocamlPackages.ocamlformat
+                ocamlPackages.utop
+                ocamlPackages.ocaml-lsp
+                ocamlPackages.angstrom
+                ocamlPackages.ppx_deriving
+              ];
+              inputsFrom = [ self'.packages.default ];
+            };
+          };
+
+          packages = {
+            default = buildDunePackage {
+              inherit version;
+              pname = name;
+              propagatedBuildInputs = with ocamlPackages; [ core ];
+              src = ./.;
+            };
+          };
+        };
+    };
+}
---
+++ 2024/lib/advent.ml
@@ -0,0 +1,4 @@
+open Core
+
+let read_lines file = In_channel.read_lines file
+let read_all file = In_channel.read_all file
